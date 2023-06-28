4e2e9e954aa1812b009e01ba37dd23b61b5fc9ea
AbdallaFathy1121 <abdallafathy1023@gmail.com>
2023-06-29 00:55:20 +0300

    BasicDesignTemplate

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,8 @@ import { Component} from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('Name', 'Description', 'https://img.delicious.com.au/fVd1u6k7/w1200/del/2022/02/chicken-chickpea-curry-163942-1.jpg'),
+    new Recipe('Name', 'Description', 'https://img.delicious.com.au/fVd1u6k7/w1200/del/2022/02/chicken-chickpea-curry-163942-1.jpg')
+  ];
 }
