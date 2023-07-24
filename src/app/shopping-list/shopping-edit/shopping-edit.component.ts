c4162e7958967a88fd4bd67e88f0a9d09a54fa8b
AbdallaFathy1121 <abdallafathy1023@gmail.com>
2023-07-24 12:30:38 +0300

    Update_ShoppingListComponent

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,17 +1,29 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  subscription!: Subscription;
+  editMode = false;
+  editedItemIndex!: number;
+
   constructor(private shoppingListService: ShoppingListService) {}
 
   ngOnInit() {
+    this.subscription = this.shoppingListService.startedEditing
+      .subscribe((index: number) => {
+        this.editMode = true;
+        this.editedItemIndex = index;
+        console.log(this.editedItemIndex);
+      });
+
   }
 
   onAddItem(form: NgForm) {
@@ -19,5 +31,10 @@ export class ShoppingEditComponent implements OnInit {
     const newIngredient = new Ingredient(value.name, value.amount);
     this.shoppingListService.addNewIngredient(newIngredient);
   }
+
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  } 
 
 }
