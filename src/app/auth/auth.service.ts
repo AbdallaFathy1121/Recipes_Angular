12d13a76d85c1c5815007564bfcfa3b94eed5b4a
AbdallaFathy1121 <abdallafathy1023@gmail.com>
2023-08-02 20:56:57 +0300

    Add_Signup_Auth_Firebase

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+interface AuthResponseData {
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    expiresIn: string;
+    localId: string;
+}
+
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+    
+    constructor(private http: HttpClient) {}
+
+    signup(email: string, password: string) {
+        return this.http.post<AuthResponseData> (
+            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCR9e9eUeyHQYcQsDnA84-bWV3J0u7oeTM',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        )
+    }
+
+
+
+}
